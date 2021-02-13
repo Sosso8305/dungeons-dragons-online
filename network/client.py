443fc6cbf7e3ecf16eae832d6b84300aabcd2128
Lucas Lévesque <lcs.levesque35@gmail.com>
2021-02-13 17:10:30 +0100

    client.py <=> interface.c

--- network/client.py
+++ network/client.py
@@ -1,22 +1,39 @@
 import socket
-
-host = "127.0.0.1"
-port = 5555
-
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect((host, port))
-print ("Connection on")
+import threading
 
 
-data="hello\n"
-socket.send(data.encode())
+class NetworkSend(threading.Thread) :
+    def __init__(self, ip, port) :
+        threading.Thread.__init__(self)
 
-while(1):
-    print("hola")
-    dataFromServer = socket.recv(1024);
-    print('he')
-    print(dataFromServer.decode())
+        self._stop_event = threading.Event()
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect((ip, port))
+        print ("Connection on")
+
+    def run(self) :
+        self.stopped = False
+        while(1):
+            message = input().encode("utf-8")
+            if self.stopped :
+                break
+            self.s.send(message)
+
+    def stop(self) :
+        self.stopped=True
 
 
-print ("Close")
-socket.close()
+
+c = NetworkSend("127.0.0.1",1234)
+c.start()
+
+while(1) :
+    data = c.s.recv(1024).decode("utf-8")
+    if not data :
+        print("Connection lost, press ENTER to exit")
+        break
+    print("recv : " + data)
+
+c.s.close()
+c.stop()
+c.join()
