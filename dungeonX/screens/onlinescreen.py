ea2181a675ffc4617e347429e93af50c08f3c8d8
Hitait Zineb <zineb.hitait@insa-cvl.fr>
2021-05-01 16:38:27 +0200

    [feat]: added format control for input *IP adress Control function *Port control (isnumeric() only) *warning message if not valid *Minor bug to fix : one must always save "optional paramaters" before moving forward

--- dungeonX/screens/onlinescreen.py
+++ dungeonX/screens/onlinescreen.py
@@ -3,6 +3,8 @@ from . import Window
 from ..graphics import Button, TextInput,TextInputOnline
 from ..map import Map
 from ..constants import TILE_WIDTH
+import ipaddress
+
 START_BUTTON_WIDTH = 200
 START_BUTTON_HEIGHT = 100
 
@@ -14,6 +16,8 @@ class OnlineScreen(Window):
         self.OpParamSaved = False
         self.__viewport = pygame.Surface((40*TILE_WIDTH, 40*TILE_WIDTH/game.RATIO))
         self.isPressed = False
+        self.isPressedN = False
+
 
 
         self.rect = self.get_rect()
@@ -22,10 +26,14 @@ class OnlineScreen(Window):
         self.background = pygame.transform.scale(self.background, (self.rect.width, self.rect.height))
         self.choiceBackground = pygame.transform.scale(pygame.image.load("dungeonX/assets/ui/map_editor/choice.png").convert(), (558, 354))
         self.choiceBackground.set_colorkey((0,0,0))
+        self.infoBackground = pygame.transform.scale(pygame.image.load("dungeonX/assets/ui/map_editor/info.png").convert(), (600, 164))
+        self.infoBackground.set_colorkey((0,0,0))
+
         self.nextButton = Button(game,(self.get_width()-2.5*START_BUTTON_WIDTH,5*self.get_height()//6-START_BUTTON_HEIGHT//2),"Next",(START_BUTTON_WIDTH, START_BUTTON_HEIGHT),action=self.saveIPaddress)
         self.OptionalButton = Button(game,(self.get_width()-1000,5*self.get_height()//6-100//2),"Optional Parameters",(200, 100),textScale=0.3) 
         self.saveButton = Button(game, (self.get_width()//2-140-5, (self.get_height()+self.choiceBackground.get_height())//2-64-15), "Save", size=(140, 64), textScale=0.3,action=self.saveOptionalParam)
         self.cancelButton = Button(game, (self.get_width()//2+5, (self.get_height()+self.choiceBackground.get_height())//2-64-15), "Cancel", size=(140, 64), imgPath="dungeonX/assets/ui/button_red.png", textScale=0.3) #action=self.cancelDialog)
+        self.okayButton = Button(game, (self.get_width()//2-75, (self.get_height()+self.infoBackground.get_height())//2-64-15), "Okay", size=(140, 64), textScale=0.3)
 
         self.IPAddressInput = TextInputOnline(game, (self.get_width()//2-325, (self.get_height())//2-75),width=15, textScale=0.6,IP=True)
         self.AddPortCInput = TextInputOnline(game, (self.get_width()//2-120, (self.get_height()+self.choiceBackground.get_height())//2-64-80),width=8,text="5555")
@@ -62,11 +70,18 @@ class OnlineScreen(Window):
             self.AddIPInput.unfocus()
         self.OpParamSaved = True
 
-    
+    def isvalidIPFormat(self,IP:str):
+        def isIPv4(s):
+         try: return str(int(s)) == s and 0 <= int(s) <= 255
+         except: return False
+        if IP.count(".") == 3 and all(isIPv4(i) for i in IP.split(".")):
+            return True
+        return False
+        
 
     def update(self, events):
         # --- Render --- #
-        if not self.isPressed:
+        if not self.isPressed and not self.isPressedN:
             self.blit(self.background, (0,0))
             self.game.textDisplayer.print("Enter your IP address To login",(0,50), rectSize=(self.get_width(),200), center=True, scale=0.55)
             self.blit(self.nextButton.image,self.nextButton.rect)
@@ -74,10 +89,7 @@ class OnlineScreen(Window):
             self.IPAddressInput.update(events)
 
             self.blit(self.IPAddressInput, self.IPAddressInput.rect)
-            self.nextButton.update(events)
-        if self.nextButton.isPressed():
-            #TODO : Condition si c'est le tout premier joueur qui lance le jeu if not then : character choice directly with map already in check !!!
-            self.game.setScreen('map_selector')
+        
         self.OptionalButton.update(events)
         if self.OptionalButton.isPressed() or self.isPressed:
             self.isPressed = True
@@ -90,18 +102,33 @@ class OnlineScreen(Window):
             self.blit(self.AddIPInput, self.AddIPInput.rect)
             self.AddPortInput.update(events, concurrentTextInputs=[self.AddIPInput, self.AddPortCInput])
             self.blit(self.AddPortInput, self.AddPortInput.rect)
-            
-            
+
             self.saveButton.update(events)
             self.blit(self.saveButton.image, self.saveButton.rect)
             if self.saveButton.isPressed():
-                #self.saveOptionalParam()
                 self.isPressed = False
+
             self.cancelButton.update(events)
             self.blit(self.cancelButton.image, self.cancelButton.rect)
             if self.cancelButton.isPressed():
                 self.isPressed = False
+        self.nextButton.update(events)
+        if self.nextButton.isPressed() or self.isPressedN:
+            #TODO : Condition si c'est le tout premier joueur qui lance le jeu if not then : character choice directly with map already in check !!!
+            if self.isvalidIPFormat(self.IPaddress) and self.isvalidIPFormat(self.IPC):
+                self.game.setScreen('map_selector')        
+            else : # Blit Real visual WARNING 
+                self.isPressedN = True
+                self.blit(self.infoBackground, (pygame.Vector2(self.game.DISPLAY_SIZE)-self.infoBackground.get_size())//2)
+                self.game.textDisplayer.print("Invalid IP address Format : example 127.0.0.1", (pygame.Vector2(self.game.DISPLAY_SIZE)-self.infoBackground.get_size())//2, rectSize=(600,80), center=True, scale=0.3)
+                print("IP adress format Invalid : example 127.0.0.1")    
+                self.okayButton.update(events)
+                self.blit(self.okayButton.image, self.okayButton.rect)
+                if self.okayButton.isPressed():
+                    self.isPressedN = False
+            #TODO: take off the fact that you always need to save OptionalParamaters 
 
+        
         if self.backbutton.isPressed():
             self.isPressed = False
         self.backbutton.update(events)
